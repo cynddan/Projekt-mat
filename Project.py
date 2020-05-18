77790d145a09f65fab3e904e5d3252203ff0ee55
cynddan <cynddan@gmail.com>
2020-05-18 23:40:59 +0200

    pochodna

--- Project.py
+++ Project.py
@@ -2,6 +2,7 @@ import sympy as sp
 import numpy as np
 
 
+liczba = False
 symbole = []
 wartosci_zmiennych = []
 pochodna = []
@@ -9,13 +10,22 @@ wartosc_symboli = []
 niepewnosci = []
 stale = []
 niepewnosc_sqr = 0
-liczba_zmiennych = int(input('liczba zmiennych: '))
+
+
+while liczba == False:
+    try:
+        liczba_zmiennych = int(input('liczba zmiennych: '))
+    except ValueError:
+        print('podaj liczbe')
+    else:
+        liczba = True
+
 wzor = sp.sympify(input('wzor: '))
 
 for i in range(liczba_zmiennych):
     symbole.append(input('zmienna: '))
     wartosci_zmiennych.append(input('jej wartosc: '))
-    niepewnosci.append(int(input('jej niepewnosc: ')))
+    niepewnosci.append(sp.sympify(input('jej niepewnosc: ')))
 
 
 for symbol_iter in range(liczba_zmiennych):
@@ -23,7 +33,6 @@ for symbol_iter in range(liczba_zmiennych):
     wartosc_symboli.append((symbole[symbol_iter],wartosci_zmiennych[symbol_iter]))
 
 for pochodna_czastkowa in range(len(pochodna)):
-    niepewnosc_sqr = niepewnosc_sqr + pochodna[pochodna_czastkowa].subs(wartosc_symboli) * niepewnosci[pochodna_czastkowa]
-
+    niepewnosc_sqr =niepewnosc_sqr + pochodna[pochodna_czastkowa].subs(wartosc_symboli) * niepewnosci[pochodna_czastkowa]
 
 sp.pprint(sp.sqrt(niepewnosc_sqr))
